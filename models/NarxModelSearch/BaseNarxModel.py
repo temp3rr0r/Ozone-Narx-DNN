e4594f245ac388e17e5aeb68fa6267b65df31ef2
temp3rr0r <madks@yahoo.gr>
2018-12-01 14:31:00 +0100

    Init.

---
+++ models/NarxModelSearch/BaseNarxModel.py
@@ -0,0 +1,311 @@
+from __future__ import print_function
+import sys
+import time
+import numpy as np
+from math import sqrt
+from sklearn.metrics import mean_squared_error
+from matplotlib import pyplot
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.models import Sequential
+from keras.layers import GaussianNoise, Dense, LSTM, Bidirectional, BatchNormalization
+from keras.utils import plot_model
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+import pandas as pd
+import gc
+from sklearn.model_selection import TimeSeriesSplit
+
+
+def trainModel(x, *args):
+    trainModel.counter += 1
+    modelLabel = trainModel.label
+    x_data, y_data = args
+    full_model_parameters = x.copy()
+
+    print("\n=============\n")
+    print("{} iteration {} using:\n\t{}".format(modelLabel, trainModel.counter, x[6:15]))
+
+    dropout1 = x[6]
+    dropout2 = x[7]
+    dropout3 = x[8]
+    recurrent_dropout1 = x[9]
+    recurrent_dropout2 = x[10]
+    recurrent_dropout3 = x[11]
+
+    # Gaussian noise
+    noise_stddev1 = x[12]
+    noise_stddev2 = x[13]
+    noise_stddev3 = x[14]
+
+    x = np.rint(x).astype(np.int32)
+    optimizers = ['adadelta', 'adagrad', 'nadam', 'adamax',
+                  'adam']  # , 'rmsprop', 'sgd'] # Avoid loss NaNs, by removing rmsprop & sgd
+    batch_size = x[0]
+    epoch_size = x[1]
+    optimizer = optimizers[x[2]]
+    units1 = x[3]
+    units2 = x[4]
+    units3 = x[5]
+
+    # Batch normalization
+    useBatchNormalization1 = x[15]
+    useBatchNormalization2 = x[16]
+    useBatchNormalization3 = x[17]
+    use_gaussian_noise1 = x[18]
+    use_gaussian_noise2 = x[19]
+    use_gaussian_noise3 = x[20]
+
+    print("\tbatch_size: {}, epoch_size: {} Optimizer: {}, LSTM Unit sizes: {} Batch Normalization/Gaussian Noise: {}"
+          .format(x[0], x[1], optimizers[x[2]], x[3:6], x[15:21]))
+
+    # x_data, x_data_holdout = x_data[:-365, :], x_data[-365:, :]
+    # y_data, y_data_holdout = y_data[:-365, :], y_data[-365:, :]
+    x_data, x_data_holdout = x_data[:-365], x_data[-365:]
+    y_data, y_data_holdout = y_data[:-365], y_data[-365:]
+
+    totalFolds = 10
+    timeSeriesCrossValidation = TimeSeriesSplit(n_splits=totalFolds)
+    # timeSeriesCrossValidation = KFold(n_splits=totalFolds)
+
+    smape_scores = []
+    mse_scores = []
+    current_fold = 0
+    for train, validation in timeSeriesCrossValidation.split(x_data, y_data):
+        current_fold += 1
+        print("Current Fold: {}/{}".format(current_fold, totalFolds))
+
+        # create model
+        model = Sequential()
+        lstm_kwargs = {'units': units1, 'dropout': dropout1, 'recurrent_dropout': recurrent_dropout1,
+                       'return_sequences': True,
+                       'implementation': 2}
+        model.add(Bidirectional(LSTM(**lstm_kwargs), input_shape=(
+        x_data.shape[1], x_data.shape[2])))  # input_shape: rows: n, timestep: 1, features: m
+
+        if use_gaussian_noise1 == 1:
+            model.add(GaussianNoise(noise_stddev1))
+        if useBatchNormalization1 == 1:
+            model.add(BatchNormalization())
+        lstm_kwargs['units'] = units2
+        lstm_kwargs['dropout'] = dropout2
+        lstm_kwargs['recurrent_dropout'] = recurrent_dropout2
+        model.add(Bidirectional(LSTM(**lstm_kwargs)))
+        if use_gaussian_noise2 == 1:
+            model.add(GaussianNoise(noise_stddev2))
+        if useBatchNormalization2 == 1:
+            model.add(BatchNormalization())
+        lstm_kwargs['units'] = units3
+        lstm_kwargs['dropout'] = dropout3
+        lstm_kwargs['recurrent_dropout'] = recurrent_dropout3
+        lstm_kwargs['return_sequences'] = False
+        model.add(Bidirectional(LSTM(**lstm_kwargs)))
+        if use_gaussian_noise3 == 1:
+            model.add(GaussianNoise(noise_stddev3))
+        if useBatchNormalization3 == 1:
+            model.add(BatchNormalization())
+        model.add(Dense(1))  # TODO: make 2x dense for prediction of 2 stuff (test[2,:]? Soil Sun + soil moisture%?
+        model.compile(loss='mean_squared_error',
+                      optimizer=optimizer)
+
+        early_stop = [EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=1, mode='auto'),
+                      ReduceLROnPlateau(patience=3, verbose=1),
+                      ModelCheckpoint(filepath='foundModels/best_model_{}.h5'.format(modelLabel), monitor='val_loss',
+                                      save_best_only=True)]
+
+        try:
+            history = model.fit(x_data[train], y_data[train],
+                                verbose=2,
+                                batch_size=batch_size,
+                                epochs=epoch_size,
+                                validation_data=(x_data[validation], y_data[validation]),
+                                callbacks=early_stop)
+        except ValueError:
+            print("Value Error exception: Model fit exception. Trying again...")
+            history = model.fit(x_data[train], y_data[train],
+                                verbose=2,
+                                batch_size=batch_size,
+                                epochs=epoch_size,
+                                validation_data=(x_data[validation], y_data[validation]),
+                                callbacks=early_stop)
+        except:
+            print("Exception: Returning max float value for this iteration.")
+
+            del model  # Manually delete model
+            from keras import backend as K
+            K.clear_session()  # Manually clear_session with keras 2.1.6
+            gc.collect()
+
+            return sys.float_info.max
+
+        prediction = model.predict(x_data[validation])
+
+        # Undo standardization
+        # y_test = (y_data[test] * sensor_std) + sensor_mean
+        # prediction = (prediction * sensor_std) + sensor_mean
+
+        y_validation = y_data[validation]
+        # Calc mse/rmse
+        mse = mean_squared_error(prediction, y_validation)
+        print('Validation MSE: %.3f' % mse)
+        mse_scores.append(mse)
+        rmse = sqrt(mse)
+        print('Validation RMSE: %.3f' % rmse)
+
+        prediction.resize((prediction.shape[0], 1))
+        y_validation.resize((prediction.shape[0], 1))
+        smape = (2.0 / prediction.shape[0]) * \
+                sum(np.nan_to_num(np.fabs(prediction - y_validation)
+                                  / (prediction + y_validation)))  # Symmetric Mean Absolute Percent Error (SMAPE)
+        print('Validation SMAPE: {}'.format(smape))
+        smape_scores.append(smape)
+
+        full_x = x_data.copy()
+        full_y = y_data.copy()
+        full_prediction = model.predict(x_data)
+        # full_expected_ts = (y_data * sensor_std) + sensor_mean
+        # full_prediction = (full_prediction * sensor_std) + sensor_mean
+        full_expected_ts = y_data
+
+        full_rmse = sqrt(mean_squared_error(full_prediction, full_expected_ts))
+        print('Full Data RMSE: %.3f' % full_rmse)
+
+        full_prediction.resize((full_prediction.shape[0], 1))
+        full_expected_ts.resize((full_prediction.shape[0], 1))
+        full_smape = (2.0 / full_prediction.shape[0]) * \
+                     sum(np.nan_to_num(np.fabs(full_prediction - full_expected_ts))
+                         / (full_prediction + full_expected_ts))  # Symmetric Mean Absolute Percent Error (SMAPE)
+        print('Full Data SMAPE: {}'.format(full_smape))
+
+
+    # Plot model architecture
+    plot_model(model, show_shapes=True, to_file='foundModels/{}ModelIter{}.png'.format(modelLabel, trainModel.counter))
+    SVG(model_to_dot(model).create(prog='dot', format='svg'))
+
+    mean_smape = np.mean(smape_scores)
+    std_smape = np.std(smape_scores)
+    print('Cross validation Full Data SMAPE: {} +/- {}'.format(round(mean_smape * 100, 2), round(std_smape * 100, 2)))
+    min_smape = pd.read_pickle("foundModels/min_smape.pkl")['min_smape'][0]
+
+    mean_mse = np.mean(mse_scores)
+    std_mse = np.std(mse_scores)
+    print('Cross validation Full Data MSE: {} +/- {}'.format(round(mean_mse * 100, 2), round(std_mse * 100, 2)))
+    min_mse = pd.read_pickle("foundModels/min_mse.pkl")['min_mse'][0]
+
+    holdout_prediction = model.predict(x_data_holdout)
+    holdout_rmse = sqrt(mean_squared_error(holdout_prediction, y_data_holdout))
+    print('Holdout Data RMSE: %.3f' % holdout_rmse)
+    holdout_prediction.resize((holdout_prediction.shape[0], 1))
+    y_data_holdout.resize((holdout_prediction.shape[0], 1))
+    holdout_smape = (2.0 / holdout_prediction.shape[0]) * \
+                    sum(np.nan_to_num(np.fabs(holdout_prediction - y_data_holdout))
+                        / (holdout_prediction + y_data_holdout))  # Symmetric Mean Absolute Percent Error (SMAPE)
+    print('Holdout Data SMAPE: {}'.format(holdout_smape))
+    holdout_mape = np.mean(np.abs((y_data_holdout - holdout_prediction) / y_data_holdout))
+    print('Holdout Data MAPE: {}'.format(holdout_mape))
+    holdout_mse = mean_squared_error(holdout_prediction, y_data_holdout)
+    print('Holdout Data MSE: {}'.format(holdout_mse))
+    # Index Of Agreement: https://cirpwiki.info/wiki/Statistics#Index_of_Agreement
+    #  IA = 1 - mean((xc(:)-xm(:)).^2)/max(mean((abs(xc(:)-mean(xm(:)))+abs(xm(:)-mean(xm(:)))).^2),eps)
+    # x_m: measured values, x_c: final calculated values
+    # holdout_ioa = 1 - ((holdout_mse) /
+    #                    (np.fabs(holdout_prediction - np.mean(holdout_prediction))
+    #                     + np.fabs(y_data_holdout - np.mean(y_data_holdout))) ** 2)
+    holdout_ioa = 1 - (np.sum((y_data_holdout - holdout_prediction) ** 2)) / (np.sum(
+        (np.abs(holdout_prediction - np.mean(y_data_holdout)) + np.abs(y_data_holdout - np.mean(y_data_holdout))) ** 2))
+    print('Holdout Data IOA: {}'.format(holdout_ioa))
+    with open('logs/{}Runs.csv'.format(modelLabel), 'a') as file:
+        # TODO: data to store:
+        # datetime, iteration, cvMseMean, cvMseStd,
+        # cvSmapeMean, cvSmapeStd, holdoutRmse, holdoutSmape, holdoutMape,
+        # holdoutMse, holdoutIoa, full_rand_parameters
+        file.write("{},{},{},{},{},{},{},{},{},{},{},{}\n".format(str(int(time.time())), str(trainModel.counter),
+                                                                  str(mean_mse), str(std_mse), str(mean_smape),
+                                                                  str(std_smape), str(holdout_rmse),
+                                                                  str(holdout_smape[0]),
+                                                                  str(holdout_mape), str(holdout_mse), str(holdout_ioa),
+                                                                  0
+                                                                  # ''.join(str(e) for e in full_rand_parameters)
+                                                                  ))
+
+    if mean_mse < min_mse:
+        print("New min_mse: {}".format(mean_mse))
+        original_df1 = pd.DataFrame({"min_mse": [mean_mse]})
+        original_df1.to_pickle("foundModels/min_mse.pkl")
+        original_df2 = pd.DataFrame({"full_{}_parameters".format(modelLabel): [full_model_parameters]})
+        original_df2.to_pickle("foundModels/full_{}_parameters.pkl".format(modelLabel))
+        model.summary()  # print layer shapes and model parameters
+
+        # Plot history
+        pyplot.figure(1, figsize=(8, 6))  # Resolution 800 x 600
+        pyplot.title("{} (iter: {}): Training History Last Fold".format(modelLabel, trainModel.counter))
+        pyplot.plot(history.history['loss'], label='loss')
+        pyplot.plot(history.history['val_loss'], label='val_loss')
+        pyplot.xlabel("Training Epoch")
+        pyplot.ylabel("MSE")
+        pyplot.grid(True)
+        pyplot.legend()
+        pyplot.show()
+
+        # Plot validation data
+        # pyplot.figure(1, figsize=(8, 6))  # Resolution 800 x 600
+        # pyplot.title("{} (iter: {}): Validation(last fold) data (RMSE: {}, SMAPE: {}%)".format(modelLabel, trainLstm.counter, np.round(rmse, 2), np.round(smape * 100, 2)))
+        # pyplot.plot(prediction, label='prediction')
+        # pyplot.plot(y_validation, label='expected')
+        # pyplot.xlabel("Time step")
+        # pyplot.ylabel("Sensor Value")
+        # pyplot.grid(True)
+        # pyplot.legend()
+        # pyplot.show()
+
+        # Plot test data
+        pyplot.figure(1, figsize=(12, 10))  # Resolution 800 x 600
+        pyplot.title("{} (iter: {}): Test data (RMSE: {}, MAPE: {}%, IOA: {}%)".format(modelLabel, trainModel.counter,
+                                                                                       np.round(holdout_rmse, 2),
+                                                                                       np.round(holdout_mape * 100, 2),
+                                                                                       np.round(holdout_ioa * 100, 2)))
+        pyplot.plot(holdout_prediction, label='prediction')
+        pyplot.plot(y_data_holdout, label='expected')
+        pyplot.xlabel("Time step")
+        pyplot.ylabel("Sensor Value")
+        pyplot.grid(True)
+        pyplot.legend()
+        pyplot.show()
+
+        # Plot full data
+        # pyplot.figure(1, figsize=(16, 12))  # Resolution 800 x 600
+        # pyplot.title("{} (iter: {}): Full data (RMSE: {}, SMAPE: {}%)".format(.format(modelLabel), trainLstm.counter, np.round(full_rmse, 2), np.round(full_smape * 100, 2)))
+        # pyplot.plot(full_prediction, label='prediction')
+        # pyplot.plot(full_expected_ts, label='expected')
+        # pyplot.xlabel("Time step")
+        # pyplot.ylabel("Sensor Value")
+        # pyplot.grid(True)
+        # pyplot.legend()
+        # pyplot.show()
+
+
+        print("New min_mse: {}".format(mean_mse))
+        original_df1 = pd.DataFrame({"min_mse": [mean_mse]})
+        original_df1.to_pickle("foundModels/min_mse.pkl")
+        original_df2 = pd.DataFrame({"full_{}_parameters".format(modelLabel): [full_model_parameters]})
+        original_df2.to_pickle("foundModels/full_{}_parameters.pkl".format(modelLabel))
+        model.summary()  # print layer shapes and model parameters
+
+        'foundModels/best_model_{}.h5'.format(modelLabel)
+
+        # Store model
+        # serialize model to JSON
+        model_json = model.to_json()
+        with open("foundModels/bestModelArchitecture.json".format(modelLabel), "w") as json_file:
+            json_file.write(model_json)
+            print("Saved model to disk")
+
+        # serialize weights to HDF5
+        model.save_weights("foundModels/bestModelWeights.h5".format(modelLabel))
+        print("Saved weights to disk")
+
+    del model  # Manually delete model
+    from keras import backend as K
+    K.clear_session()  # Manually clear_session with keras 2.1.6
+    gc.collect()
+
+    return mean_mse
---
+++ models/NarxModelSearch/ModelSearch.py
@@ -0,0 +1,100 @@
+from __future__ import print_function
+import random
+from scipy.optimize import differential_evolution
+from BaseNarxModel import trainModel
+from pyswarm.pso import pso
+from scipy.optimize import basinhopping
+
+# Model Search Space bounds
+bounds = [(7, 512),  # batch_size
+          (100, 300), (0, 3),  # , 5)    # epoch_size, optimizer
+          (16, 256), (16, 256), (16, 256),  # TODO: 1024, 1024, 1024  # units
+          (0.01, 0.52), (0.01, 0.25), (0.01, 0.25),  # dropout
+          (0.01, 0.25), (0.01, 0.25), (0.01, 0.25),  # recurrent_dropout
+          (0.01, 1), (0.01, 1), (0.01, 1),  # gaussian noise std
+          (0, 1), (0, 1), (0, 1),  # gaussian_noise
+          (0, 1), (0, 1), (0, 1)]  # batch normalization
+
+# Lower Bounds
+lb = [bounds[0][0],  # batch_size
+      bounds[1][0], bounds[2][0],  # epoch_size, optimizer
+      bounds[3][0], bounds[4][0], bounds[5][0],  # units
+      bounds[6][0], bounds[7][0], bounds[8][0],  # dropout
+      bounds[9][0], bounds[10][0], bounds[11][0],  # recurrent_dropout
+      bounds[12][0], bounds[13][0], bounds[14][0],  # gaussian noise std
+      bounds[15][0], bounds[16][0], bounds[17][0],  # gaussian_noise
+      bounds[18][0], bounds[19][0], bounds[20][0]]  # batch normalization
+
+# Upper Bounds
+ub = [bounds[0][1],  # batch_size
+      bounds[1][1], bounds[2][1],  # epoch_size, optimizer
+      bounds[3][1], bounds[4][1], bounds[5][1],  # units
+      bounds[6][1], bounds[7][1], bounds[8][1],  # dropout
+      bounds[9][1], bounds[10][1], bounds[11][1],  # recurrent_dropout
+      bounds[12][1], bounds[13][1], bounds[14][1],  # gaussian noise std
+      bounds[15][1], bounds[16][1], bounds[17][1],  # gaussian_noise
+      bounds[18][1], bounds[19][1], bounds[20][1]]  # batch normalization
+
+
+def randomModelSearch(x_data, y_data, iterations=100):
+
+    args = (x_data, y_data)
+    trainModel.counter = 0  # Function call counter
+    trainModel.label = 'rand'
+    for i in range(iterations):
+        trainModel(getRandomModel(), *args)
+
+def getRandomModel():
+    return [random.randint(lb[0], ub[0]),  # batch_size
+             random.randint(lb[1], ub[1]), random.randint(lb[2], ub[2]),  # epoch_size, optimizer
+             random.randint(lb[3], ub[3]), random.randint(lb[4], ub[4]), random.randint(lb[5], ub[5]),  # units
+             random.uniform(lb[6], ub[6]), random.uniform(lb[7], ub[7]), random.uniform(lb[8], ub[8]),  # dropout
+             random.uniform(lb[9], ub[9]), random.uniform(lb[10], ub[10]), random.uniform(lb[11], ub[11]),  # recurrent_dropout
+             random.uniform(lb[12], ub[12]), random.uniform(lb[13], ub[13]), random.uniform(lb[14], ub[14]),  # gaussian noise std
+             random.randint(lb[15], ub[15]), random.randint(lb[16], ub[16]), random.randint(lb[17], ub[17]), # gaussian_noise
+             random.randint(lb[18], ub[18]), random.randint(lb[19], ub[19]), random.randint(lb[20], ub[20])]
+
+def printOptimum(xopt1, fopt1):
+    print('The optimum is at:')
+    print('    {}'.format(xopt1))
+    print('Optimal function value:')
+    print('    myfunc: {}'.format(fopt1))
+
+
+def differentialEvolutionModelSearch(x_data, y_data):
+
+    args = (x_data, y_data)
+    trainModel.counter = 0  # Function call counter
+    trainModel.label = 'de'
+    xopt1, fopt1 = differential_evolution(trainModel, bounds, args=args)  # TODO: test DE params
+    printOptimum(xopt1, fopt1)
+
+
+def particleSwarmOptimizationModelSearch(x_data, y_data):
+
+    args = (x_data, y_data)
+    trainModel.counter = 0  # Function call counter
+    trainModel.label = 'pso'
+    xopt1, fopt1 = pso(trainModel, lb, ub, args=args)
+    # TODO: test larger swarm, more iterations
+    # pso(func, lb, ub, ieqcons=[], f_ieqcons=None, args=(), kwargs={},
+    #     swarmsize=100, omega=0.5, phip=0.5, phig=0.5, maxiter=100, minstep=1e-8,
+    #     minfunc=1e-8, debug=False)
+    printOptimum(xopt1, fopt1)
+
+
+def basinHoppingpModelSearch(x_data, y_data):
+
+    args = (x_data, y_data)
+    x0 = getRandomModel()
+    bounds = [(low, high) for low, high in zip(lb, ub)]  # rewrite the bounds in the way required by L-BFGS-B
+
+    # TODO: check minimisers: https://docs.scipy.org/doc/scipy/reference/optimize.html
+    minimizer_kwargs = dict(method="TNC", bounds=bounds, args=args)  # TODO: test method = "SLSQP". TODO: test Jacobian methods from minimizers
+    # minimizer_kwargs = dict(method="L-BFGS-B", bounds=bounds, args=y_test)  # use method L-BFGS-B because the problem is smooth and bounded
+    # minimizer_kwargs = dict(method="SLSQP", bounds=bounds, args=y_test)
+
+    trainModel.counter = 0  # Function call counter
+    trainModel.label = 'bh'
+    res = basinhopping(trainModel, x0, minimizer_kwargs=minimizer_kwargs)
+    print(res)
