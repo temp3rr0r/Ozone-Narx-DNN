46464ff5c71a3d3625e8d4b32622fe0fd7aba273
temp3rr0r <madks@yahoo.gr>
2018-12-31 03:20:48 +0100

    Multi competing worker + distinct islands results queues + common task queue works in sample (blocking were it should) :). Before changing it on src.

--- models/NarxModelSearch/new_task.py
+++ models/NarxModelSearch/new_task.py
@@ -1,24 +1,47 @@
 import pika
-import sys
 import json
-# import numpy as np
+import random
+import uuid
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 channel = connection.channel()
-channel.queue_declare(queue="task_queue", durable=True)
+channel.queue_declare(queue="task_queue", durable=False)
 
-message = " ".join(sys.argv[1:]) or "Hello World!"
+island = random.choice(["pso", "rand", "de"])
+results_queue = "results_queue" + "_" + island + "_" + str(uuid.uuid4())[:5]
+channel.queue_declare(queue=results_queue, durable=False)
 
-message = "long........."
-message = "short."
-message = "error..."
 
-array1 = [234.2, 34, 0.005, 0.18, 1.0, 99]
-message = json.dumps(array1)
+def callback(ch, method, properties, body):
+    try:
+        body = json.loads(body)
+        print(" [x] Received %r" % body)
+        print(" [x] Received mse: ", body["mse"])
+        print(" [x] Done")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        channel.stop_consuming()
+    except ValueError as ev:  # Handle exceptions
+        print(" [x] Exception, sending rejection %s" % str(ev))
+        ch.basic_reject(delivery_tag=method.delivery_tag)
 
-channel.basic_publish(exchange="", routing_key="task_queue", body=message,
+
+channel.basic_qos(prefetch_count=1)
+
+for i in range(0, 5):
+
+    array1 = {"array": [random.uniform(0, 1), random.uniform(100, 300)], "delay": ["."] * random.randint(0, 8),
+              "island": island, "results_queue": results_queue}
+    message = json.dumps(array1)
+    channel.basic_publish(exchange="", routing_key="task_queue", body=message,
                       properties=pika.BasicProperties(delivery_mode=2))  # make msg persistent
-print(" [x] Sent '%s'" % message)
+    channel.basic_consume(callback, queue=results_queue)
+    print(" [x] Sent '%s'" % message)
+    channel.start_consuming()
+print(" [*] Waiting for messages. To exit press CTRL+C")
 
+channel.queue_delete(queue=results_queue)
 connection.close()
+
+#
+
 
--- models/NarxModelSearch/worker.py
+++ models/NarxModelSearch/worker.py
@@ -5,22 +5,38 @@ import numpy as np
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 channel = connection.channel()
-channel.queue_declare(queue="task_queue", durable=True)
-
+channel.queue_declare(queue="task_queue", durable=False)
+# channel.queue_declare(queue="results_queue", durable=False)
+results_queues = []
 
 def callback(ch, method, properties, body):
     try:
         body = json.loads(body)
         print(" [x] Received %r" % body)
-        time.sleep(body.count(b"."))
+        time.sleep(str(body["delay"]).count("."))
 
         # Do work
-        array1 = np.array(body)
-        mse = array1 * 3
-        print(" [x] mse: ", np.mean(mse))
+        array1 = np.array(body["array"])
+        mse = np.mean(array1 * 3)
+        print(" [x] mse: ", mse)
 
-        print(" [x] Done")
+        # TODO: add queue of results in case it doesn't exist
+        # island = body["island"]
+        results_queue = body["results_queue"]
+        if not any(results_queue in s for s in results_queues):
+            results_queues.append(results_queue)
+            channel.queue_declare(queue=results_queue, durable=False)
+
         ch.basic_ack(delivery_tag=method.delivery_tag)
+
+        # TODO: send back result
+        mse_message = {"array1": array1.tolist(), "mse": mse.tolist(), "island": body["island"]}
+        mse_message = json.dumps(mse_message)
+        channel.basic_publish(exchange="", routing_key=results_queue, body=mse_message,
+                              properties=pika.BasicProperties(delivery_mode=2))  # make msg persistent
+        print(" [x] Sent back'%s'" % mse_message)
+        print(" [x] Done")
+
     except ValueError as ev:  # Handle exceptions
         print(" [x] Exception, sending rejection %s" % str(ev))
         ch.basic_reject(delivery_tag=method.delivery_tag)
