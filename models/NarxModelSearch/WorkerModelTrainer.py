87ee83f828e7c6ee866a6e0adec4de1bd26e5f4b
temp3rr0r <madks@yahoo.gr>
2019-02-24 02:12:25 +0100

    O3 46 stations 1

--- models/NarxModelSearch/WorkerModelTrainer.py
+++ models/NarxModelSearch/WorkerModelTrainer.py
@@ -161,7 +161,7 @@ def model_training_callback(ch, method, properties, body):  # Tasks receiver cal
         ch.basic_reject(delivery_tag=method.delivery_tag)
 
 
-gpu_device = 2  # Set GPU
+gpu_device = 1  # Set GPU
 
 print("--- Loading GPU {}...".format(gpu_device))
 init_gpu(gpu_device)
