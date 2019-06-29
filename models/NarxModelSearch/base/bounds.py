8fecf00941978d2c847978023926d3bd71d7ed24
temp3rr0r <madks@yahoo.gr>
2019-06-29 02:52:14 +0200

    Bounds 21:end don't causes exceptions. Before testing 0:2

--- models/NarxModelSearch/base/bounds.py
+++ models/NarxModelSearch/base/bounds.py
@@ -1,18 +1,32 @@
+bounds = [(7, 1 * 31),  # batch_size (~ #days: week, month, year)
+          (350, 600),  # epoch_size
+          (0, 4),  # optimizer
+          (64, 512),  # units
+          (64, 512),
+          (64, 512),
+          (0.01, 0.25),  # dropout
+          (0.01, 0.25),
+          (0.01, 0.25),
+          (0.01, 0.25),  # recurrent_dropout
+          (0.01, 0.25),
+          (0.01, 0.25),
+          (0.01, 1),  # gaussian noise std
+          (0.01, 1),
+          (0.01, 1),
+          (0, 1),  # gaussian_noise
+          (0, 1),
+          (0, 1),
+          (0, 1),  # batch normalization
+          (0, 1),
+          (0, 1),
+          (0, 5),  # base layer types (plain/bidirectional: LSTM, GRU, Simple RNN)
+          (0, 5),
+          (0, 5),
+          (0, 9),  # layer initializers, normal/uniform he/lecun,...
+          (0, 9),
+          (0, 9)]
 
 # Model Search Space bounds
-bounds = [(7, 1 * 31),  # batch_size (~ #days: week, month, year)
-          (350, 600), (0, 4),  # , 5)    # epoch_size, optimizer
-          # (1023, 1024), (1023, 1024), (1023, 1024),  # units
-          (64, 512), (64, 512), (64, 512),
-          # (32, 512), (32, 196), (32, 384),
-          (0.01, 0.25), (0.01, 0.25), (0.01, 0.25),  # dropout
-          (0.01, 0.25), (0.01, 0.25), (0.01, 0.25),  # recurrent_dropout
-          (0.01, 1), (0.01, 1), (0.01, 1),  # gaussian noise std
-          (0, 1), (0, 1), (0, 1),  # gaussian_noise
-          (0, 1), (0, 1), (0, 1),  # batch normalization
-          (0, 5), (0, 5), (0, 5),  # base layer types (plain/bidirectional: LSTM, GRU, Simple RNN)
-          (0, 9), (0, 9), (0, 9)]  # layer initializers, normal/uniform he/lecun,...
-
 lb, ub = zip(*bounds)
 lb = list(lb)  # Lower Bounds
 ub = list(ub)  # Upper Bounds
