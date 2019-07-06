999322fbb9d39227b527b41820952257d9c3304d
temp3rr0r <madks@yahoo.gr>
2019-07-07 00:03:52 +0200

    Added parametrized unit tests for 1D to 3D CA indexing. Before applying it on mpi src code.

--- models/NarxModelSearch/CellularAutomata/cellular_automata_indexing.py
+++ models/NarxModelSearch/CellularAutomata/cellular_automata_indexing.py
@@ -1,81 +1,83 @@
 import numpy as np
 
-debug = True
-def convert_1D_to_nD_index(rank, size, cellular_automata_dimensions):
-    returning_nD_index = []
-    n = len(cellular_automata_dimensions)
-    ca = cellular_automata_dimensions[0]
-    for j in range(1, n):
-        ca *= cellular_automata_dimensions[j]
-    for i in range(1, n - 1):
-        ca /= cellular_automata_dimensions[n - i]
-        returning_nD_index.append(int(rank / ca))
-        rank = int(rank % ca)
-    returning_nD_index.append(int(rank % cellular_automata_dimensions[1]))
-    returning_nD_index.append(int(rank / cellular_automata_dimensions[1]))
-    returning_nD_index.reverse()
-    return returning_nD_index
 
+class CellularAutomataIndexing:
 
-def convert_nD_to_1D_index(index_nD, size, cellular_automata_dimensions):
-    returning_1D_index = 0
-    n = len(cellular_automata_dimensions)
-    ca = cellular_automata_dimensions[0]
-    for i in range(2, n):
-        ca *= cellular_automata_dimensions[i - 1]
-        returning_1D_index += index_nD[-(n - i)] * ca
-    returning_1D_index += index_nD[-n] * cellular_automata_dimensions[1]
-    returning_1D_index += index_nD[-(n - 1)]
-    return returning_1D_index
+    def __init__(self):
+        pass
+        # rank, size, cellular_automata_dimensions
+        # index_nD,
+        # population_count, selection_pressure=1.5):
+        # grid_mse_rank_list
 
+    def convert_1D_to_nD_index(self, rank, size, cellular_automata_dimensions):
+        returning_nD_index = []
+        n = len(cellular_automata_dimensions)
+        ca = cellular_automata_dimensions[0]
+        for j in range(1, n):
+            ca *= cellular_automata_dimensions[j]
+        for i in range(1, n - 1):
+            ca /= cellular_automata_dimensions[n - i]
+            returning_nD_index.append(int(rank / ca))
+            rank = int(rank % ca)
+        returning_nD_index.append(int(rank % cellular_automata_dimensions[1]))
+        returning_nD_index.append(int(rank / cellular_automata_dimensions[1]))
+        returning_nD_index.reverse()
+        return returning_nD_index
 
-def get_1D_neighbours(rank, size, cellular_automata_dimensions):
-    returning_1D_neighbours = []
-    index_nD = convert_1D_to_nD_index(rank, size, cellular_automata_dimensions)
-    for idx in range(len(cellular_automata_dimensions)):
-        new_nD = index_nD.copy()
-        new_nD[idx] += 1
-        if new_nD[idx] >= cellular_automata_dimensions[idx]:
-            new_nD[idx] = 0
-        else:
-            new_1D = convert_nD_to_1D_index(new_nD, size, cellular_automata_dimensions)
-            if new_1D >= size:
+    def convert_nD_to_1D_index(self, index_nD, size, cellular_automata_dimensions):
+        returning_1D_index = 0
+        n = len(cellular_automata_dimensions)
+        ca = cellular_automata_dimensions[0]
+        for i in range(2, n):
+            ca *= cellular_automata_dimensions[i - 1]
+            returning_1D_index += index_nD[-(n - i)] * ca
+        returning_1D_index += index_nD[-n] * cellular_automata_dimensions[1]
+        returning_1D_index += index_nD[-(n - 1)]
+        return returning_1D_index
+
+    def get_1D_neighbours(self, rank, size, cellular_automata_dimensions):
+        returning_1D_neighbours = []
+        index_nD = self.convert_1D_to_nD_index(rank, size, cellular_automata_dimensions)
+        for idx in range(len(cellular_automata_dimensions)):
+            new_nD = index_nD.copy()
+            new_nD[idx] += 1
+            if new_nD[idx] >= cellular_automata_dimensions[idx]:
                 new_nD[idx] = 0
-        new_1D = convert_nD_to_1D_index(new_nD, size, cellular_automata_dimensions)
-        returning_1D_neighbours.append(new_1D)
-    return returning_1D_neighbours
+            else:
+                new_1D = self.convert_nD_to_1D_index(new_nD, size, cellular_automata_dimensions)
+                if new_1D >= size:
+                    new_nD[idx] = 0
+            new_1D = self.convert_nD_to_1D_index(new_nD, size, cellular_automata_dimensions)
+            returning_1D_neighbours.append(new_1D)
+        return returning_1D_neighbours
 
+    def get_linear_ranking_selection_probabilities(self, population_count, selection_pressure=1.5):
+        returning_probabilities = []
+        s = selection_pressure
+        mu = population_count
+        for i in range(population_count - 1, -1, -1):
+            returning_probabilities.append((2 - s) / mu + (2 * i * (s - 1)) / (mu * (mu - 1)))
+        return np.array(returning_probabilities)
 
-def get_linear_ranking_selection_probabilities(population_count, selection_pressure=1.5):
-    returning_probabilities = []
-    s = selection_pressure
-    mu = population_count
-    for i in range(population_count - 1, -1, -1):
-        returning_probabilities.append((2 - s) / mu + (2 * i * (s - 1)) / (mu * (mu - 1)))
-    return np.array(returning_probabilities)
+    def get_cellular_automata_linear_selection_neighbour_1D_index(self, rank, size, cellular_automata_dimensions, grid_mse_rank_list):
 
-
-def get_cellular_automata_linear_selection_neighbour_1D_index(rank, size, cellular_automata_dimensions, grid_mse_rank_list):
-
-    if len(cellular_automata_dimensions) > 1:
-        oneD_neighbours = get_1D_neighbours(rank, size, cellular_automata_dimensions)
-        if debug:
-            print("oneD_neighbours: {}".format(oneD_neighbours))
-        mu = len(oneD_neighbours)
-        s = 1.5  # 1 < s <= 2: 1 means NO pressure at all, 2 means worst aint's selected
-        idx = np.random.choice(mu, 1, p=get_linear_ranking_selection_probabilities(mu, s))
-        neighborhood_mse = []
-        for oneD_index in oneD_neighbours:
-            neighborhood_mse.append(grid_mse_rank_list[oneD_index])
-        neighborhood_mse.sort()
-        selected_neighborhood_mse = neighborhood_mse[idx[0]]
-        return grid_mse_rank_list.index(selected_neighborhood_mse)
-    else:
-        next_index = rank + 1
-        if next_index > size - 1:
-            next_index = 0
-        return next_index
-
+        if len(cellular_automata_dimensions) > 1:
+            oneD_neighbours = self.get_1D_neighbours(rank, size, cellular_automata_dimensions)
+            mu = len(oneD_neighbours)
+            s = 1.5  # 1 < s <= 2: 1 means NO pressure at all, 2 means worst aint's selected
+            idx = np.random.choice(mu, 1, p=self.get_linear_ranking_selection_probabilities(mu, s))
+            neighborhood_mse = []
+            for oneD_index in oneD_neighbours:
+                neighborhood_mse.append(grid_mse_rank_list[oneD_index])
+            neighborhood_mse.sort()
+            selected_neighborhood_mse = neighborhood_mse[idx[0]]
+            return grid_mse_rank_list.index(selected_neighborhood_mse)
+        else:
+            next_index = rank + 1
+            if next_index > size - 1:
+                next_index = 0
+            return next_index
 
 if __name__ == '__main__':
 
@@ -85,7 +87,9 @@ if __name__ == '__main__':
     current_mpi_rank = 15
     mpi_size = len(agentsBuffer_mse)
     CA_dimensions = [5, 4]
-    lr_selected_rank = get_cellular_automata_linear_selection_neighbour_1D_index(current_mpi_rank, mpi_size, CA_dimensions, agentsBuffer_mse)
+    cellularAutomataIndexing = CellularAutomataIndexing()
+    lr_selected_rank = cellularAutomataIndexing.get_cellular_automata_linear_selection_neighbour_1D_index(
+        current_mpi_rank, mpi_size, CA_dimensions, agentsBuffer_mse)
     for i in range(len(agentsBuffer_mse)):
         if i % CA_dimensions[1] == 0:
             print("\n")
@@ -104,7 +108,7 @@ if __name__ == '__main__':
     current_mpi_rank = 17
     mpi_size = len(agentsBuffer_mse)
     CA_dimensions = [22]
-    lr_selected_rank = get_cellular_automata_linear_selection_neighbour_1D_index(current_mpi_rank, mpi_size, CA_dimensions, agentsBuffer_mse)
+    lr_selected_rank = cellularAutomataIndexing.get_cellular_automata_linear_selection_neighbour_1D_index(current_mpi_rank, mpi_size, CA_dimensions, agentsBuffer_mse)
 
     print("CA dimensions: {}".format(CA_dimensions))
     print("mpi_size: {}".format(mpi_size))
--- models/NarxModelSearch/CellularAutomata/test_cellular_automata_indexing.py
+++ models/NarxModelSearch/CellularAutomata/test_cellular_automata_indexing.py
@@ -1,37 +1,64 @@
 import pytest
+from cellular_automata_indexing import CellularAutomataIndexing
 
-# TODO: input train range
-class InsufficientAmount(Exception):
-    pass
-
-
-class Wallet:
-    def __init__(self, amount=0):
-        self.balance = amount
-
-    def add_cash(self, amount):
-        self.balance += amount
-
-    def spend_cash(self, amount):
-        if self.balance < amount:
-            raise InsufficientAmount("Exception (InsufficientAmount): Balance {} < amount {}."
-                                     .format(self.balance, amount))
-        self.balance -= amount
 
 @pytest.fixture
-def empty_wallet():
-    """
-    Instantiates an empty wallet.
-    :return: An empty Wallet object.
-    """
-    return Wallet()
+def cellular_automata_indexing():
+    return CellularAutomataIndexing()
 
 
+@pytest.mark.parametrize(
+    'agents_buffer_mse, mpi_size, ca_dimensions, lr_selected_rank_list, mse_list, current_mpi_rank', [
+        ([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117],
+         18, [3, 3, 3], [1, 3, 9], [101, 103, 109], 0),
+        ([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117],
+         18, [3, 3, 3], [2, 4, 10], [102, 104, 110], 1)
+    ])
+def test_3D_indexing(cellular_automata_indexing, agents_buffer_mse, mpi_size, ca_dimensions, lr_selected_rank_list,
+                     mse_list, current_mpi_rank):
+    mse_index = cellular_automata_indexing.get_cellular_automata_linear_selection_neighbour_1D_index(
+        current_mpi_rank, mpi_size, ca_dimensions, agents_buffer_mse)
+    assert mse_index in lr_selected_rank_list
+    assert agents_buffer_mse[mse_index] in mse_list
 
-def test_default_initial_amount(empty_wallet):
-    """
-    Checks if an empty wallet object has a balance of zero.
-    :param empty_wallet: A wallet object.
-    """
-    assert empty_wallet.balance == 0
 
+@pytest.mark.parametrize(
+    'agents_buffer_mse, mpi_size, ca_dimensions, lr_selected_rank_list, mse_list, current_mpi_rank', [
+        ([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117],
+         18, [5, 4], [3, 12], [103, 112], 15),
+        ([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117],
+         18, [5, 4], [1, 4], [101, 104], 0),
+        ([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117],
+         18, [5, 4], [10, 13], [110, 113], 9),
+        ([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117],
+         18, [5, 4], [14, 17], [114, 117], 13),
+        ([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117],
+         18, [5, 4], [0, 17], [100, 117], 16),
+        ([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117],
+         18, [5, 4], [1, 16], [101, 116], 17)
+    ])
+def test_2D_indexing(cellular_automata_indexing, agents_buffer_mse, mpi_size, ca_dimensions, lr_selected_rank_list,
+                     mse_list, current_mpi_rank):
+    mse_index = cellular_automata_indexing.get_cellular_automata_linear_selection_neighbour_1D_index(
+        current_mpi_rank, mpi_size, ca_dimensions, agents_buffer_mse)
+    assert mse_index in lr_selected_rank_list
+    assert agents_buffer_mse[mse_index] in mse_list
+
+
+@pytest.mark.parametrize(
+    'agents_buffer_mse, mpi_size, ca_dimensions, lr_selected_rank_list, mse_list, current_mpi_rank', [
+        ([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117],
+         18, [22], [0], [100], 17),
+        ([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117],
+         18, [22], [1], [101], 0),
+        ([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117],
+         18, [22], [17], [117], 16),
+        ([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117],
+         18, [22], [13], [113], 12)
+    ])
+def test_1D_indexing(cellular_automata_indexing, agents_buffer_mse, mpi_size, ca_dimensions, lr_selected_rank_list,
+                     mse_list, current_mpi_rank):
+    mse_index = cellular_automata_indexing.get_cellular_automata_linear_selection_neighbour_1D_index(
+        current_mpi_rank, mpi_size, ca_dimensions, agents_buffer_mse)
+    assert mse_index in lr_selected_rank_list
+    assert agents_buffer_mse[mse_index] in mse_list
