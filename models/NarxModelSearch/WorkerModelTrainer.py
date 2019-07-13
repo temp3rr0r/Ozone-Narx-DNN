2f7af47311547d317c142ca99162b3143fc11479
temp3rr0r <madks@yahoo.gr>
2019-07-13 07:51:38 +0200

    O3 model search seems that it can start. Starting tests on O3 NAS with 3x3x3 CA Linear ranking selection 2. Before setting up TX2 and P100 from vsc.

--- models/NarxModelSearch/WorkerModelTrainer.py
+++ models/NarxModelSearch/WorkerModelTrainer.py
@@ -118,7 +118,12 @@ def load_data(directory, file_prefix, mimo_outputs, gpu_rank=1):
 
     # TODO: greatly decrease r length for testing: 2010-2017 training, 2018 for testing
     # TODO: PM10 from 1995
-    row2010_01_01 = 5481 - 1
+    # row2010_01_01 = 5481 - 1
+
+    # TODO: greatly decrease r length for testing: 2010-2017 training, 2018 for testing
+    # TODO: O3 from 1995
+    row2010_01_01 = 7307 - 1
+
     r = r[row2010_01_01:-1, :]
 
     print("r[0, 0]", r[0, 0])
