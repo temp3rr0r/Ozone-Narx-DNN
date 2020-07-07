b057a20bdee87ea844b3b785060cbd0dd3f5a19c
temp3rr0r <madks@yahoo.gr>
2020-07-07 10:10:59 +0100

    Also calculating IOA% for GPR, SVM, Ensembles and Tree

--- models/Matlab/AirQualityNARX/modelTestAccuracy.m
+++ models/Matlab/AirQualityNARX/modelTestAccuracy.m
@@ -21,30 +21,46 @@ X_test.Properties.VariableNames{'Var9'} = 'VarName9';
 %% Naive-1
 y_test_prediction = [y_test_matrix(1); y_test_matrix(1:end-1)];
 MAPE = mean((abs(y_test_prediction - y_test_matrix))./y_test_matrix);
-disp("Naive-1 MAPE: " + round(MAPE * 100,2 ) + "% (Bayesian optimization 100 iters)")
+IOA = index_of_agreement(y_test_matrix, y_test_prediction);
+disp("Naive-1 MAPE: " + round(MAPE * 100, 2) + "% IOA: " + round(IOA * 100, 2) + "% (Bayesian optimization 100 iters)")
 
 %% Tree
 load('best_Tree_Bayes100.mat');
 y_test_prediction = trainedTreeBayes100.predictFcn(X_test);
 MAPE = mean((abs(y_test_prediction - y_test_matrix))./y_test_matrix);
-disp("Tree MAPE: " + round(MAPE * 100,2 ) + "% (Bayesian optimization 100 iters)")
+IOA = index_of_agreement(y_test_matrix, y_test_prediction);
+disp("Tree MAPE: " + round(MAPE * 100, 2) + "% IOA: " + round(IOA * 100, 2) + "% (Bayesian optimization 100 iters)")
 
 %% Ensemble
 load('best_Ensemble_Bayes100.mat');
 y_test_prediction = trainedEnsembleBayes100.predictFcn(X_test);
 MAPE = mean((abs(y_test_prediction - y_test_matrix))./y_test_matrix);
-disp("Ensemble MAPE: " + round(MAPE * 100,2 ) + "% (Bayesian optimization 100 iters)")
+IOA = index_of_agreement(y_test_matrix, y_test_prediction);
+disp("Ensemble MAPE: " + round(MAPE * 100, 2) + "% IOA: " + round(IOA * 100, 2) + "% (Bayesian optimization 100 iters)")
 
 
 %% Gaussian SVM
 load('best_Gaussian_SVM_Bayes100.mat');
 y_test_prediction = trainedGaussianSVMBayes100.predictFcn(X_test);
 MAPE = mean((abs(y_test_prediction - y_test_matrix))./y_test_matrix);
-disp("Gaussian SVM MAPE: " + round(MAPE * 100,2 ) + "% (Bayesian optimization 100 iters)")
+IOA = index_of_agreement(y_test_matrix, y_test_prediction);
+disp("Gaussian SVM MAPE: " + round(MAPE * 100, 2) + "% IOA: " + round(IOA * 100, 2) + "% (Bayesian optimization 100 iters)")
 
 
 %% GPR
 load('best_GPR_Bayes100.mat');
 y_test_prediction = trainedGPRBayes100.predictFcn(X_test);
 MAPE = mean((abs(y_test_prediction - y_test_matrix))./y_test_matrix);
-disp("GPR MAPE: " + round(MAPE * 100,2 ) + "% (Bayesian optimization 100 iters)")
+IOA = index_of_agreement(y_test_matrix, y_test_prediction);
+disp("GPR MAPE: " + round(MAPE * 100, 2) + "% IOA: " + round(IOA * 100, 2) + "% (Bayesian optimization 100 iters)")
+
+
+%%
+function ioa = index_of_agreement(validation, prediction)
+    
+    % Calculates Index Of Agreement (IOA).
+    % :param validation: actual values
+    % :param prediction: predicted values
+    % :return: IOA float.   
+    ioa =  1 - (sum((validation - prediction) .^ 2)) / (sum((abs(prediction - mean(validation)) + abs(validation - mean(validation))) .^ 2));
+end
