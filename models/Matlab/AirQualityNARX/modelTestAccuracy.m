d6cf1f550a23a94dc003ab1e826817fa8283fa6c
temp3rr0r <madks@yahoo.gr>
2020-07-07 12:51:05 +0100

    Added train/test indices in .m

--- models/Matlab/AirQualityNARX/modelTestAccuracy.m
+++ models/Matlab/AirQualityNARX/modelTestAccuracy.m
@@ -6,6 +6,12 @@ T = readtable('6vars/BETN073.csv');
 X_test = T(7306:7676, 3:9);
 y_test = T(7306:7676, 2);
 
+
+%% Load training data
+% Indices: B3653:I7305
+X_train = T(3653:7305, 3:9);
+y_train = T(3653:7305, 2);
+
 %%
 X_test_matrix = table2array(X_test);
 y_test_matrix = table2array(y_test);
@@ -19,6 +25,7 @@ X_test.Properties.VariableNames{'Var7'} = 'e11';
 X_test.Properties.VariableNames{'Var8'} = 'VarName8';
 X_test.Properties.VariableNames{'Var9'} = 'VarName9';
 %% Naive-1
+disp("10-fold cross-validation");
 y_test_prediction = [y_test_matrix(1); y_test_matrix(1:end-1)];
 MAPE = mean((abs(y_test_prediction - y_test_matrix))./y_test_matrix);
 IOA = index_of_agreement(y_test_matrix, y_test_prediction);
