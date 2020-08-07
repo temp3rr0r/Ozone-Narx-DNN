58286f8c95435e849baf6f2a2235f15a51be2572
temp3rr0r <madks@yahoo.gr>
2020-08-07 14:47:02 +0200

    RS5 stored. Started Island18_1.

--- models/NarxModelSearch/mpiNeuroevolutionIslands.py
+++ models/NarxModelSearch/mpiNeuroevolutionIslands.py
@@ -69,8 +69,8 @@ if os.path.exists("foundModels/best_model_parameters.pkl"):
 
 # First island in vector is not considered
 # islands = ['ls'] * 6  # Local search islands
-# islands = ['pso', 'ga', 'bo', 'de', 'rand'] * 7  # TODO: test/debug DA islands
-islands = ['rand'] * 20
+islands = ['pso', 'ga', 'bo', 'de', 'rand'] * 7  # TODO: test/debug DA islands
+# islands = ['rand'] * 20
 # islands = ['bo'] * 7  # TODO: test/debug DA islands
 # islands = ['pso', 'ga', 'bo', 'de', 'rand', 'ls'] * 7  # TODO: LS island =OUT=> global islands
 
